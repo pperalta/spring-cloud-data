014c8cd4627dc2b14a207133903d9b574a177523
Patrick Peralta <pperalta@pivotal.io>
2015-08-03 15:21:46 -0400

    Controller wip

---
+++ spring-cloud-data-rest/src/main/java/org/springframework/cloud/data/rest/controller/AdminController.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.data.rest.controller;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.data.rest.resource.StreamDefinitionResource;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Patrick Peralta
+ */
+@Controller
+@RequestMapping("/")
+public class AdminController {
+
+	private final EntityLinks entityLinks;
+
+	@Autowired
+	public AdminController(EntityLinks entityLinks) {
+		this.entityLinks = entityLinks;
+	}
+
+	@RequestMapping()
+	@ResponseBody
+	public ResourceSupport info() {
+		RootResource resourceSupport = new RootResource();
+
+		resourceSupport.add(entityLinks.linkFor(StreamDefinitionResource.class).withRel("streams"));
+
+		return resourceSupport;
+	}
+
+	@XmlRootElement
+	public static class RootResource extends ResourceSupport {
+	}
+}
